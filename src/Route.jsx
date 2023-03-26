b7e19e55530e345fa932bc2693b3826e8ac30d79
leonwgc <leon.wang@derbysoft.net>
2023-03-26 16:19:21 +0800

    feat: immer

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,18 +1,22 @@
-import {lazy} from 'react';
+import { lazy } from 'react';
 
 const routes = [
-    {
-        path: '/Popover',
-        component: lazy(() => import('./demos/Popover'))
-    },
-    {
-        path: '/Accordion',
-        component: lazy(() => import('./demos/Accordion'))
-    },
-    {
-        path: '/Table',
-        component: lazy(() => import('./demos/Table'))
-    }
+  {
+    path: '/Popover',
+    component: lazy(() => import('./demos/Popover')),
+  },
+  {
+    path: '/Accordion',
+    component: lazy(() => import('./demos/Accordion')),
+  },
+  {
+    path: '/Table',
+    component: lazy(() => import('./demos/Table')),
+  },
+  {
+    path: '/Immer',
+    component: lazy(() => import('./demos/ImmerJs')),
+  },
 ];
 
 export default routes;
---
+++ src/demos/ImmerJs.jsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from 'react';
+import { Button, Collapse } from 'react-uni-comps';
+import { produce } from 'immer';
+
+const baseState = [
+  {
+    title: 'Learn TypeScript',
+    desc: 'Learn TypeScript desc',
+  },
+  {
+    title: 'Try Immer',
+    desc: 'Try Immer desc',
+  },
+];
+
+export default function App() {
+  const ref = useRef();
+
+  const [list, setList] = useState(baseState);
+
+  return (
+    <div>
+      <Collapse>
+        {list.map(item => (
+          <Collapse.Item title={item.title}>{item.desc}</Collapse.Item>
+        ))}
+      </Collapse>
+      <Button
+        type="primary"
+        style={{ marginTop: 20 }}
+        onClick={() => {
+          setList(
+            produce(draft => {
+              const nextId = draft.length + 1;
+              draft.push({
+                title: 'title ' + nextId,
+                desc: 'desc ' + nextId,
+              });
+            })
+          );
+        }}
+      >
+        Add more
+      </Button>
+    </div>
+  );
+}
