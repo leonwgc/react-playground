b6839f3e8e8f318ed37107f7511c7cf9ab0acaf6
leonwgc <leon.wang@derbysoft.net>
2023-08-24 11:31:53 +0800

    chore: update table demo

--- src/demos/Table.jsx
+++ src/demos/Table.jsx
@@ -1,107 +1,53 @@
 import React, { useState, useCallback, useRef } from 'react';
 import { useMount, useUpdateEffect } from 'react-uni-comps';
 import Table from 'alcedo-ui/Table';
+import TextField from 'alcedo-ui/TextField';
 import { get } from '~/utils/req';
 import { useAppData, useUpdateStore } from '~/redux';
 
 export default function App() {
-  const udpateStore = useUpdateStore();
-  const {
-    updatedClickNegativeKeywordsList = [],
-    clickNegativeKeywordsValue = [], // selected id array
-  } = useAppData();
-
-  const [currentClickNegativeKeywordsList, setCurrentClickNegativeKeywordsList] = useState([]);
-
-  const ref = useRef(); // hold data list
-
-  useMount(() => {
-    get('/dplatform-cloud-gateway/click-ppc/preset-negative-keywords?languageCode=en').then(res => {
-      // presetClickNegativeKeywords
-      const formatedClickNegativeKeywordData = res.data?.map(item => ({
-        ifSelected: false,
-        id: item.tagCategory,
-        content: item.tagCategory,
-        'match type': 'Broad Match',
-        children: item.tags.map(tag => ({
-          ...tag,
-          ifSelected: clickNegativeKeywordsValue.includes(tag.id),
-        })),
-      }));
-
-      ref.current = formatedClickNegativeKeywordData;
-
-      setCurrentClickNegativeKeywordsList(formatedClickNegativeKeywordData);
-    });
-  });
-
-  useUpdateEffect(() => {
-    const formatedClickNegativeKeywordData = ref.current?.map(item => ({
-      ...item,
-      children: item.children.map(tag => ({
-        ...tag,
-        ifSelected: clickNegativeKeywordsValue.includes(tag.id),
-      })),
-    }));
-
-    setCurrentClickNegativeKeywordsList(formatedClickNegativeKeywordData);
-
-    const selectedKeywords = formatedClickNegativeKeywordData?.map(item => {
-      const selectedChildren = item.children
-        ?.filter(child => !!child.ifSelected)
-        .flat()
-        .map(({ ifSelected, ...item }) => item);
-      const parentSelected = item.children.length === selectedChildren.length;
-      if (parentSelected) {
-        return [
-          {
-            content: item.content,
-            id: item.content,
-            ['match type']: 'Broad Match',
-          },
-          ...selectedChildren,
-        ].flat();
-      } else {
-        return selectedChildren;
-      }
-    });
-
-    udpateStore({
-      updatedClickNegativeKeywordsList: selectedKeywords?.flat(),
-    });
-  }, [clickNegativeKeywordsValue]);
-
-  const handleCheckboxChange = useCallback(selectedKeywords => {
-    const selectedKeywordsIds = selectedKeywords
-      .filter(keyword => !keyword.children) // only children id
-      .map(keyword => keyword.id);
-    udpateStore({ clickNegativeKeywordsValue: selectedKeywordsIds });
-  }, []);
-
-  const clickNegativeKeywordColumns = [
+  const [data, setData] = useState([
     {
-      key: 'negative-keywords',
+      name: 'hotel name',
+      text: 'Title',
+      id: 1,
+      english: '',
+      children: [
+        { name: '', text: 'Url', id: 11, english: '' },
+        { name: '', text: 'Description1', id: 12, english: '' },
+        { name: '', text: 'Description2', id: 13, english: '' }
+      ]
+    }
+  ]);
+
+  const columns = [
+    {
+      key: 'name',
       align: Table.Align.LEFT,
-      headRenderer: 'Negative Keywords',
-      bodyClassName: 'negative-keywords',
-      bodyRenderer: rowData => rowData.content,
-      sortable: true,
-      sortingProp: 'content',
+      headRenderer: 'name',
+      bodyRenderer: (rowData) => rowData.name
     },
     {
-      key: 'match-type',
+      key: 'text',
       align: Table.Align.LEFT,
-      headRenderer: 'Match Type',
-      bodyClassName: 'match-type',
-      bodyRenderer: rowData => rowData['match type'],
-      sortingProp: 'match-type',
+      headRenderer: 'Text',
+      bodyRenderer: (rowData) => rowData.text
     },
+    {
+      key: 'english',
+      align: Table.Align.LEFT,
+      headRenderer: 'English',
+      bodyRenderer: (rowData) => (
+        <TextField
+          value={rowData.english}
+          onChange={(v) => {
+            rowData.english = v;
+            setData([...data]);
+          }}
+        />
+      )
+    }
   ];
-
-  const checkedKeywordList = updatedClickNegativeKeywordsList?.map(item => ({
-    ...item,
-    ifSelected: true,
-  }));
 
   return (
     <div>
@@ -109,15 +55,10 @@ export default function App() {
         idField="content"
         isHeadFixed={true}
         isPaginated={false}
-        canBeExpanded={true}
-        isSelectRecursive={true}
-        value={checkedKeywordList}
-        checked={value => value.ifSelected}
-        columns={clickNegativeKeywordColumns}
-        data={currentClickNegativeKeywordsList}
+        canBeExpanded
+        columns={columns}
+        data={data}
         selectMode={Table.SelectMode.MULTI_SELECT}
-        className="click-negative-keywords__table"
-        onChange={value => handleCheckboxChange(value)}
       />
     </div>
   );
