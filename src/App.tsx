06ada9ac2b73dada7db3a1f9a2ef7215a6beb5a5
leonwgc <leon.wang@derbysoft.net>
2023-08-24 17:24:27 +0800

    chore: bump up ruc

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { ThemeProvider, styled } from 'react-uni-comps';
+import { ThemeProvider, styled, StyleSheetManager } from 'react-uni-comps';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import routes from './Route';
 import Widget from 'alcedo-ui/Widget';
@@ -18,35 +18,38 @@ const StyledWrapper = styled.div`
 
 export default function App() {
     return (
-        <ThemeProvider color={'#005cff'}>
-            <HashRouter>
-                <Suspense fallback={null}>
-                    <StyledWrapper>
-                        <Widget>
-                            <WidgetHeader title="React Playgound">
-                                <IconButton iconCls="far fa-thumbs-up" />
-                                <IconButton iconCls="fas fa-trash-alt" />
-                            </WidgetHeader>
+        /** show css rules */
+        <StyleSheetManager disableCSSOMInjection>
+            <ThemeProvider color={'#005cff'}>
+                <HashRouter>
+                    <Suspense fallback={null}>
+                        <StyledWrapper>
+                            <Widget>
+                                <WidgetHeader title="React Playgound">
+                                    <IconButton iconCls="far fa-thumbs-up" />
+                                    <IconButton iconCls="fas fa-trash-alt" />
+                                </WidgetHeader>
 
-                            <div className="widget-content">
-                                <Switch>
-                                    {routes.map((route, idx) => (
+                                <div className="widget-content">
+                                    <Switch>
+                                        {routes.map((route, idx) => (
+                                            <Route
+                                                key={idx}
+                                                path={route.path}
+                                                exact={route.exact}
+                                                component={route.component}
+                                            />
+                                        ))}
                                         <Route
-                                            key={idx}
-                                            path={route.path}
-                                            exact={route.exact}
-                                            component={route.component}
+                                            render={() => <div>demo not found</div>}
                                         />
-                                    ))}
-                                    <Route
-                                        render={() => <div>demo not found</div>}
-                                    />
-                                </Switch>
-                            </div>
-                        </Widget>
-                    </StyledWrapper>
-                </Suspense>
-            </HashRouter>
-        </ThemeProvider>
+                                    </Switch>
+                                </div>
+                            </Widget>
+                        </StyledWrapper>
+                    </Suspense>
+                </HashRouter>
+            </ThemeProvider>
+        </StyleSheetManager>
     );
 }
