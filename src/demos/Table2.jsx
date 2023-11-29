e49e188bb22784ebe2b93d4c0e74975ffb27e3f2
leonwgc <leon.wang@derbysoft.net>
2023-11-29 13:31:17 +0800

    chore: try in sort

--- src/demos/Table2.jsx
+++ src/demos/Table2.jsx
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Table from 'alcedo-ui/Table';
+import Sortable from 'sortablejs';
+import { Icon, styled, Space, Input, createGlobalStyle } from 'react-uni-comps';
 
-import { Icon, styled, Space, Input } from 'react-uni-comps';
+const GlobalStyle = createGlobalStyle`
+.sortable-chosen {
+  opacity: .8;
+}
+.sortable-chosen .hand{
+  cursor: grab;
+}
+.sortable-ghost {
+  border-style: dashed;
+  border-width: 1px;
+  opacity: 0.4;
+}
+`;
+
+const StyledHand = styled.div`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  border: 1px solid #999;
+  cursor: grab;
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 4px;
+
+  &:active {
+    cursor: grabbing;
+  }
+`;
 
 const EditableText = (props) => {
   const { isEdit = false, value, onChange, onSubmit, ...rest } = props;
@@ -42,14 +71,19 @@ const StyledTable = styled(Table)`
 export default function App() {
   const [data, setData] = useState([
     {
-      name: 'hotel2',
-      id: 1,
+      name: 'hotel1',
+      id: '1',
       msg: 'hi'
     },
     {
       name: 'hotel2',
-      id: 2,
+      id: '2',
       msg: 'hi2'
+    },
+    {
+      name: 'hotel3',
+      id: '3',
+      msg: 'hi'
     }
   ]);
 
@@ -58,7 +92,12 @@ export default function App() {
       key: 'name',
       align: Table.Align.LEFT,
       headRenderer: 'name',
-      bodyRenderer: (rowData) => rowData.name
+      bodyRenderer: (rowData) => (
+        <span>
+          <StyledHand className="hand" />
+          {rowData.name}
+        </span>
+      )
     },
     {
       key: 'text',
@@ -98,5 +137,44 @@ export default function App() {
     }
   ];
 
-  return <StyledTable isPaginated={false} columns={columns} data={data} />;
+  const ref = useRef();
+
+  React.useEffect(() => {
+    const tbody = ref.current.content.current.wrapperEl.querySelector('tbody');
+    if (tbody) {
+      const els = Array.prototype.slice.call(tbody.children, 1);
+      els.map((el, index) => {
+        el.setAttribute('data-id', data[index].id);
+      });
+    }
+    let s = Sortable.create(ref.current.content.current.wrapperEl.querySelector('tbody'), {
+      // dataIdAttr: 'data-id',
+      handle: '.hand',
+      store: {
+        set: function (ss) {
+          const ar = ss.toArray().slice(1);
+          const newList = data.sort((a, b) => ar.indexOf(a.id) - ar.indexOf(b.id));
+          setData([...newList]);
+        }
+      }
+    });
+
+    return () => {
+      s.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <GlobalStyle />
+      <StyledTable
+        isPaginated={false}
+        columnKeyField="id"
+        idField="id"
+        columns={columns}
+        data={data}
+        ref={ref}
+      />
+    </div>
+  );
 }
