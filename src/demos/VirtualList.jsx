7b2951f38930117144ff9548452d3dad1ae38833
leonwgc <leon.wang@derbysoft.net>
2024-04-11 15:33:14 +0800

    chore: virtual list demo

--- src/demos/VirtualList.jsx
+++ src/demos/VirtualList.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { styled } from 'react-uni-comps';
+import { styled, useEventListener } from 'react-uni-comps';
 import List from 'rc-virtual-list';
 
-const data = new Array(100).fill().map((e, i) => i + 1);
+const data = new Array(1000).fill().map((e, i) => i + 1);
 
 const StyledList = styled.div`
   box-sizing: border-box;
   padding: 1px;
   width: 200px;
+  height: 400px;
   border: 1px solid #ccc;
 
   .item {
     box-sizing: border-box;
-    height: 60px;
+    height: 40px;
     border: 1px dashed #00bc8d;
     display: flex;
     justify-content: center;
@@ -21,18 +22,36 @@ const StyledList = styled.div`
 `;
 
 export default function VirtualListDemo() {
+  const ref = React.useRef();
+  const distance = React.useRef(0);
+  useEventListener(ref, 'wheel', (e) => {
+    distance.current += e.deltaY;
+
+    console.log(distance.current);
+  });
   return (
     <div>
       <h3>virtual list from Ali</h3>
-      <div>
+      <div style={{ display: 'flex' }}>
         <StyledList>
-          <List data={data} height={400} itemHeight={60} component={StyledList} itemKey="id">
+          <List
+            data={data}
+            height={400}
+            itemHeight={40}
+            component={StyledList} // default: 'div'
+            styles={{ verticalScrollBar: { display: 'none' } }} // hide vertical scrollbar
+            extraRender={(info) => <div>this is end</div>}
+          >
             {(item) => (
               <div className="item" key={item}>
                 {item}
               </div>
             )}
           </List>
+        </StyledList>
+
+        <StyledList ref={ref} style={{ overflow: 'scroll' }}>
+          <div style={{ height: '100vh' }}></div>
         </StyledList>
       </div>
     </div>
