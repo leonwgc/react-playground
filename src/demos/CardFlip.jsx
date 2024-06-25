41150b66cd67d5cb49401e2801a63bf0c7ee9ec8
leonwgc <leon.wang@derbysoft.net>
2024-06-25 16:54:11 +0800

    refactor: card flip

--- src/demos/CardFlip.jsx
+++ src/demos/CardFlip.jsx
@@ -2,48 +2,42 @@ import React, { useState } from 'react';
 import { clsx, styled } from 'react-uni-comps';
 
 const StyledContainer = styled.div`
-  width: 200px;
-  height: 200px;
-  perspective: 600px;
-
-  .cube {
-    width: 100%;
-    height: 100%;
-    position: relative;
-    transform-style: preserve-3d;
-    border: 1px solid #eee;
-    transform: rotateY(0deg);
-    transition: all ease-out 0.6s;
-
-    &.clicked {
-      transform: rotateY(180deg);
-    }
-  }
-
-  .cube__face {
-    position: absolute;
+  .scene {
     width: 200px;
     height: 200px;
-    background-color: red;
-    color: #fff;
-    font-size: 50px;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
+    perspective: 600px;
 
-    &.front {
-      background-color: red;
-    }
-    &.back {
-      background-color: blue;
-      transform: rotateY(180deg);
-    }
-    backface-visibility: hidden;
+    .card {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      transform-style: preserve-3d;
+      border: 1px solid #eee;
+      transition: all ease-out 0.6s;
 
-    &.clicked {
-      /* transform: rotateY(5deg) skewY(2deg) translateZ(20px);
-      font-size: 60px;
-      filter: brightness(1.1); */
+      &.clicked {
+        transform: rotateY(180deg);
+      }
+
+      .face {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        color: #fff;
+        font-size: 50px;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        backface-visibility: hidden;
+
+        &.front {
+          background-color: red;
+        }
+        &.back {
+          background-color: blue;
+          transform: rotateY(180deg);
+        }
+      }
     }
   }
 `;
@@ -51,10 +45,12 @@ const StyledContainer = styled.div`
 export default function Test() {
   const [clicked, setCilcked] = useState(false);
   return (
-    <StyledContainer onClick={() => setCilcked((c) => !c)}>
-      <div className={clsx('cube', { clicked: clicked })}>
-        <div className={clsx('cube__face front')}>front</div>
-        <div className={clsx('cube__face back')}>back</div>
+    <StyledContainer>
+      <div className="scene" onClick={() => setCilcked((c) => !c)}>
+        <div className={clsx('card', { clicked: clicked })}>
+          <div className={clsx('face back')}>back</div>
+          <div className={clsx('face front')}>front</div>
+        </div>
       </div>
     </StyledContainer>
   );
