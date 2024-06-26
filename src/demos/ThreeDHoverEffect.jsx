c0e95c0927ba43172f8e22938985425f3499fb90
leonwgc <leon.wang@derbysoft.net>
2024-06-26 17:10:10 +0800

    feat: add ArcHoverEffect

--- src/demos/ThreeDHoverEffect.jsx
+++ src/demos/ThreeDHoverEffect.jsx
@@ -1,80 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { clsx, styled } from 'react-uni-comps';
+import { clsx, styled, createGlobalStyle } from 'react-uni-comps';
+import ArcHoverEffect from '../components/ArcHoverEffect';
 
-const StyledContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
+// ref: https://codepen.io/technokami/pen/abojmZa
 
-  .ef-scene {
-    perspective: 800px;
-    .ef-object {
-      transform-style: preserve-3d;
-      position: relative;
+const StyledItem = styled.div`
+  display: inline-block;
+  height: 200px;
+  width: 300px;
+  transition: box-shadow 0.1s, transform 0.1s;
+  background-image: url(https://img1.baidu.com/it/u=1109106620,180722445&fm=253&fmt=auto&app=138&f=JPEG?w=786&h=500);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
 
-      .ef-face {
-        display: inline-block;
-        height: 200px;
-        width: 300px;
-        transition: box-shadow 0.2s, transform 0.2s;
-        background-image: url(https://img1.baidu.com/it/u=1109106620,180722445&fm=253&fmt=auto&app=138&f=JPEG?w=786&h=500);
-        background-size: 100% 100%;
-        background-repeat: no-repeat;
-
-        &:hover {
-          box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.6);
-          cursor: pointer;
-        }
-      }
-    }
+  &:hover {
+    box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.6);
+    cursor: pointer;
   }
 `;
 
+const GlobalStyle = createGlobalStyle`
+   body{
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+   }
+`;
+
 export default function Test() {
-  useEffect(() => {
-    let el = document.querySelector('.ef-object');
-
-    const height = el.clientHeight;
-    const width = el.clientWidth;
-
-    el.addEventListener('mousemove', handleMove);
-
-    /* Define function a */
-    function handleMove(e) {
-      const xVal = e.layerX;
-      const yVal = e.layerY;
-      /*
-       * Calculate rotation valuee along the Y-axis
-       * Here the multiplier 20 is to
-       * Control the rotation
-       */
-      const yRotation = 20 * ((xVal - width / 2) / width);
-      const xRotation = -20 * ((yVal - height / 2) / height);
-      const string = 'rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg) scale(1.1)';
-      e.currentTarget.style.transform = string;
-    }
-
-    el.addEventListener('mouseout', function (e) {
-      e.currentTarget.style.transform = ' scale(1) rotateX(0) rotateY(0)';
-    });
-
-    el.addEventListener('mousedown', function (e) {
-      e.currentTarget.style.transform = 'scale(0.9) rotateX(0) rotateY(0)';
-    });
-
-    el.addEventListener('mouseup', function (e) {
-      e.currentTarget.style.transform = 'scale(1.1) rotateX(0) rotateY(0)';
-    });
-  }, []);
   return (
-    <StyledContainer>
-      <div className="ef-scene">
-        <div className="ef-object">
-          <div className="ef-face"></div>
-        </div>
-      </div>
-    </StyledContainer>
+    <StyledItem>
+      <GlobalStyle />
+      <ArcHoverEffect>
+        <StyledItem />
+      </ArcHoverEffect>
+    </StyledItem>
   );
 }
