c6763883672bca26b6b07dcf6cf2e50da24b40ff
leonwgc <leon.wang@derbysoft.net>
2024-11-04 18:02:49 +0800

    refactor: use WeakMap to reuse proxy

--- src/hooks/useReactive.jsx
+++ src/hooks/useReactive.jsx
@@ -1,31 +1,39 @@
 import { useMemo, useRef } from 'react';
 import { useForceUpdate } from 'react-uni-comps';
 
+const map = new WeakMap();
+
 const getProxyObject = (obj, forceUpdate) => {
-  return new Proxy(obj, {
-    get: (target, prop, receiver) => {
-      const rt = Reflect.get(target, prop, receiver);
-      if (typeof rt === 'object' && rt) {
-        return getProxyObject(rt, forceUpdate);
+  if (map.has(obj)) {
+    return map.get(obj);
+  } else {
+    const proxy = new Proxy(obj, {
+      get: (target, prop, receiver) => {
+        const rt = Reflect.get(target, prop, receiver);
+        if (typeof rt === 'object' && rt) {
+          return getProxyObject(rt, forceUpdate);
+        }
+        return rt;
+      },
+      set: (target, prop, value, receiver) => {
+        let rt;
+        if (typeof value === 'object' && value) {
+          rt = Reflect.set(target, prop, getProxyObject(value, forceUpdate), receiver);
+        } else {
+          rt = Reflect.set(target, prop, value, receiver);
+        }
+        forceUpdate();
+        return rt;
+      },
+      deleteProperty: (target, prop) => {
+        const rt = Reflect.deleteProperty(target, prop);
+        forceUpdate();
+        return rt;
       }
-      return rt;
-    },
-    set: (target, prop, value, receiver) => {
-      let rt;
-      if (typeof value === 'object' && value) {
-        rt = Reflect.set(target, prop, getProxyObject(value, forceUpdate), receiver);
-      } else {
-        rt = Reflect.set(target, prop, value, receiver);
-      }
-      forceUpdate();
-      return rt;
-    },
-    deleteProperty: (target, prop) => {
-      const rt = Reflect.deleteProperty(target, prop);
-      forceUpdate();
-      return rt;
-    }
-  });
+    });
+    map.set(obj, proxy);
+    return proxy;
+  }
 };
 
 export const useReactive = (initialValue) => {
