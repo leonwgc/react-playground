9fb89a0ac4ca8062290234a2145a2dc89a5d3ea5
leonwgc <leon.wang@derbysoft.net>
2024-11-21 17:18:45 +0800

    refactor: add types

--- src/demos/UseAnimateDotCSSDemo.jsx
+++ src/demos/UseAnimateDotCSSDemo.jsx
@@ -2,16 +2,11 @@ import React, { useState } from 'react';
 import 'animate.css';
 import useAnimateDotCSS from '../hooks/useAnimateDotCSS';
 
-const effects = [
+const allAnimationEffects = [
   'bounce',
   'flash',
   'pulse',
   'rubberBand',
-  'bounceIn',
-  'bounceInDown',
-  'bounceInLeft',
-  'bounceInRight',
-  'bounceInUp',
   'shakeX',
   'shakeY',
   'headShake',
@@ -20,6 +15,24 @@ const effects = [
   'wobble',
   'jello',
   'heartBeat',
+  'backInDown',
+  'backInLeft',
+  'backInRight',
+  'backInUp',
+  'backOutDown',
+  'backOutLeft',
+  'backOutRight',
+  'backOutUp',
+  'bounceIn',
+  'bounceInDown',
+  'bounceInLeft',
+  'bounceInRight',
+  'bounceInUp',
+  'bounceOut',
+  'bounceOutDown',
+  'bounceOutLeft',
+  'bounceOutRight',
+  'bounceOutUp',
   'fadeIn',
   'fadeInDown',
   'fadeInDownBig',
@@ -32,18 +45,72 @@ const effects = [
   'fadeInTopLeft',
   'fadeInTopRight',
   'fadeInBottomLeft',
-  'fadeInBottomRight'
+  'fadeInBottomRight',
+  'fadeOut',
+  'fadeOutDown',
+  'fadeOutDownBig',
+  'fadeOutLeft',
+  'fadeOutLeftBig',
+  'fadeOutRight',
+  'fadeOutRightBig',
+  'fadeOutUp',
+  'fadeOutUpBig',
+  'fadeOutTopLeft',
+  'fadeOutTopRight',
+  'fadeOutBottomRight',
+  'fadeOutBottomLeft',
+  'flip',
+  'flipInX',
+  'flipInY',
+  'flipOutX',
+  'flipOutY',
+  'lightSpeedInRight',
+  'lightSpeedInLeft',
+  'lightSpeedOutRight',
+  'lightSpeedOutLeft',
+  'rotateIn',
+  'rotateInDownLeft',
+  'rotateInDownRight',
+  'rotateInUpLeft',
+  'rotateInUpRight',
+  'rotateOut',
+  'rotateOutDownLeft',
+  'rotateOutDownRight',
+  'rotateOutUpLeft',
+  'rotateOutUpRight',
+  'hinge',
+  'jackInTheBox',
+  'rollIn',
+  'rollOut',
+  'zoomIn',
+  'zoomInDown',
+  'zoomInLeft',
+  'zoomInRight',
+  'zoomInUp',
+  'zoomOut',
+  'zoomOutDown',
+  'zoomOutLeft',
+  'zoomOutRight',
+  'zoomOutUp',
+  'slideInDown',
+  'slideInLeft',
+  'slideInRight',
+  'slideInUp',
+  'slideOutDown',
+  'slideOutLeft',
+  'slideOutRight',
+  'slideOutUp'
 ];
 
 let i = 0;
 
 export default () => {
-  const [animation, setAnimation] = useState(effects[0]);
+  const [animation, setAnimation] = useState(allAnimationEffects[0]);
 
   const ref = useAnimateDotCSS(
     animation,
     () => {
-      setAnimation(effects[++i % effects.length]);
+      setAnimation(allAnimationEffects[++i % allAnimationEffects.length]);
     },
     {
       duration: '600ms',
---
+++ src/hooks/useAnimateDotCSS.tsx
@@ -0,0 +1,156 @@
+import { useState } from 'react';
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
+
+type AnimateEffectNames =
+  | 'bounce'
+  | 'flash'
+  | 'pulse'
+  | 'rubberBand'
+  | 'shakeX'
+  | 'shakeY'
+  | 'headShake'
+  | 'swing'
+  | 'tada'
+  | 'wobble'
+  | 'jello'
+  | 'heartBeat'
+  | 'backInDown'
+  | 'backInLeft'
+  | 'backInRight'
+  | 'backInUp'
+  | 'backOutDown'
+  | 'backOutLeft'
+  | 'backOutRight'
+  | 'backOutUp'
+  | 'bounceIn'
+  | 'bounceInDown'
+  | 'bounceInLeft'
+  | 'bounceInRight'
+  | 'bounceInUp'
+  | 'bounceOut'
+  | 'bounceOutDown'
+  | 'bounceOutLeft'
+  | 'bounceOutRight'
+  | 'bounceOutUp'
+  | 'fadeIn'
+  | 'fadeInDown'
+  | 'fadeInDownBig'
+  | 'fadeInLeft'
+  | 'fadeInLeftBig'
+  | 'fadeInRight'
+  | 'fadeInRightBig'
+  | 'fadeInUp'
+  | 'fadeInUpBig'
+  | 'fadeInTopLeft'
+  | 'fadeInTopRight'
+  | 'fadeInBottomLeft'
+  | 'fadeInBottomRight'
+  | 'fadeOut'
+  | 'fadeOutDown'
+  | 'fadeOutDownBig'
+  | 'fadeOutLeft'
+  | 'fadeOutLeftBig'
+  | 'fadeOutRight'
+  | 'fadeOutRightBig'
+  | 'fadeOutUp'
+  | 'fadeOutUpBig'
+  | 'fadeOutTopLeft'
+  | 'fadeOutTopRight'
+  | 'fadeOutBottomRight'
+  | 'fadeOutBottomLeft'
+  | 'flip'
+  | 'flipInX'
+  | 'flipInY'
+  | 'flipOutX'
+  | 'flipOutY'
+  | 'lightSpeedInRight'
+  | 'lightSpeedInLeft'
+  | 'lightSpeedOutRight'
+  | 'lightSpeedOutLeft'
+  | 'rotateIn'
+  | 'rotateInDownLeft'
+  | 'rotateInDownRight'
+  | 'rotateInUpLeft'
+  | 'rotateInUpRight'
+  | 'rotateOut'
+  | 'rotateOutDownLeft'
+  | 'rotateOutDownRight'
+  | 'rotateOutUpLeft'
+  | 'rotateOutUpRight'
+  | 'hinge'
+  | 'jackInTheBox'
+  | 'rollIn'
+  | 'rollOut'
+  | 'zoomIn'
+  | 'zoomInDown'
+  | 'zoomInLeft'
+  | 'zoomInRight'
+  | 'zoomInUp'
+  | 'zoomOut'
+  | 'zoomOutDown'
+  | 'zoomOutLeft'
+  | 'zoomOutRight'
+  | 'zoomOutUp'
+  | 'slideInDown'
+  | 'slideInLeft'
+  | 'slideInRight'
+  | 'slideInUp'
+  | 'slideOutDown'
+  | 'slideOutLeft'
+  | 'slideOutRight'
+  | 'slideOutUp';
+
+const prefix = 'animate__';
+
+/**
+ * Animate a node with animate.css
+ *
+ * @param {Element} node The node to animate
+ * @param {string} animation The animation name to use
+ * @return {Promise}
+ */
+const animateCSS = (node, animation) =>
+  new Promise((resolve) => {
+    const animationName = `${prefix}${animation}`;
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, { once: true });
+  });
+
+/**
+ * Apply animation to element with animate.css.
+ * @param animation
+ * @param animationEndCallback
+ * @param customProperties
+ * @returns
+ */
+const useAnimationCSS = (
+  animation: AnimateEffectNames = 'fadeIn',
+  animationEndCallback,
+  customProperties: Record<string, string> = {}
+) => {
+  const [node, ref] = useState(null);
+
+  useIsomorphicLayoutEffect(() => {
+    if (node instanceof HTMLElement) {
+      if (customProperties) {
+        Object.entries(customProperties).forEach(([key, value]) => {
+          node.style.setProperty(`--animate-${key}`, value);
+        });
+      }
+
+      animateCSS(node, animation).then(animationEndCallback);
+    }
+  }, [node, animation]);
+
+  return ref;
+};
+
+export default useAnimationCSS;
