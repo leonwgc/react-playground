5aa39218416d5c67e007f2905b5b66dd11569b3d
leonwgc <leon.wang@derbysoft.net>
2024-12-11 12:11:48 +0800

    refactor: add pack bar

--- pack.js
+++ pack.js
@@ -4,20 +4,25 @@ const argv = require('yargs').argv;
 
 const build = !!argv.build;
 
-pack(!build, {
-  entry: {
-    index: `./src/index`
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    port: 9100,
-    historyApiFallback: true
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src')
+pack(
+  !build,
+  {
+    entry: {
+      index: `./src/index`
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      port: 9100,
+      historyApiFallback: true
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
     }
-  }
-});
+  },
+  null,
+  'react-play'
+);
