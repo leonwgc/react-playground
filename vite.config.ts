69aa660050b480759298550788c593f8ff9fc018
leonwgc <giantfish@126.com>
2025-01-11 16:39:51 +0800

    fix: sass issues

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, 'node_modules')
+        '~': path.resolve(__dirname, 'src')
       }
     },
     server: {
