188afed9ef4f2bae65e7d802d89adf3335142ed1
leonwgc <giantfish@126.com>
2025-01-12 18:07:08 +0800

    feat: packrs

--- rpack.js
+++ rpack.js
@@ -2,8 +2,6 @@ const { run, build } = require('packrs');
 const path = require('path');
 
 run({
-  dev: true,
-  port: 9100,
   banner: 'react-playground',
   index: './src/index',
   dist: './dist/dist',
